b1b5da714f2536746ab7a3e9ad8574d318d2a876
Simone <286290@studenti.unimore.it>
2025-03-15 18:23:16 +0100

    Comment out get_external_ip function and its usage in app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ client = MongoClient(uri, server_api=ServerApi('1'))
 
 import requests
 
-def get_external_ip():
+"""def get_external_ip():
     response = requests.get("https://api64.ipify.org?format=json")
     if response.status_code == 200:
         data = response.json()
@@ -16,7 +16,7 @@ def get_external_ip():
         return "Unknown"
 
 external_ip = get_external_ip()
-st.write("External IP:", external_ip)
+st.write("External IP:", external_ip)"""
 
 
 db = client['Chinook']
