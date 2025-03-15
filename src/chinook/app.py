c700aeb61985abae1ff412604dfd13cc1a50fbf7
Simone <286290@studenti.unimore.it>
2025-03-15 16:20:54 +0100

    Passaggio a streamlit

--- src/chinook/app.py
+++ src/chinook/app.py
@@ -1,188 +1,75 @@
-from flask import Flask, render_template, request
 from pymongo import MongoClient
+import streamlit as st
 
-app = Flask(__name__)
-
-# Connessione al database
 client = MongoClient("mongodb://localhost:27017/")
 db = client["Chinook_optimized"]
 
-@app.route('/')
-def home():
-    return render_template('index.html')
+st.title("Chinook Database Query Selector")
 
-@app.route('/query', methods=['POST'])
-def query():
-    # Otteniamo il tipo di query selezionato
-    query_type = request.form['query_type']
+# Definisci le query come funzioni
+def count_tracks():
+    return db.Track.count_documents({}), ["Number of Tracks"]
 
-    if query_type == "count_tracks":
-        result = db.Track.count_documents({})
-        mongo_query = "db.Track.count_documents({})"
-        result_data = [{"Risultato": result}]
-    elif query_type == "count_album":
-        result = len(db.Track.distinct("Album.AlbumId"))
-        mongo_query = "db.Track.distinct('Album.AlbumId')"
-        result_data = [{"Risultato": result}]
-    elif query_type == "count_genres":
-        result = len(db.Track.distinct("Genre.GenreId"))
-        mongo_query = "db.Track.distinct('Genre.GenreId')"
-        result_data = [{"Risultato": result}]
-    elif query_type == "count_artists":
-        result = len(db.Track.distinct("Artist.ArtistId"))
-        mongo_query = "db.Track.distinct('Artist.ArtistId')"
-        result_data = [{"Risultato": result}]
-    elif query_type == "count_playlists":
-        result = len(db.Track.distinct("Playlist.PlaylistId"))
-        mongo_query = "db.Track.distinct('Playlist.PlaylistId')"
-        result_data = [{"Risultato": result}]
-    elif query_type == "songs_per_album":
-        result = db.Track.aggregate([
-            {
-                "$group": {
-                    "_id": "$Album.Title",
-                    "count": {"$sum": 1}
-                }
-            },
-            {
-                "$project": {
-                    "_id": 0,
-                    "value": "$_id",
-                    "count": 1
-                }
-            },
-            {
-                "$sort": {"count": -1}
-            }
-        ])
-        mongo_query = "db.Track.aggregate([{$group: {_id: '$Album.Title', count: {$sum: 1}}, {$project: {_id: 0, Album: '$_id', Count: 1}}, {$sort: {Count: -1}}])"
-        result_data = list(result)
-        return render_template('results_2.html', result_data=result_data, mongo_query=mongo_query, column_titles=["Album", "Numero di Canzoni"])
-    elif query_type == "songs_per_genre":
-        result = db.Track.aggregate([
-            {
-                "$group": {
-                    "_id": "$Genre.Name",
-                    "count": {"$sum": 1}
-                }
-            },
-            {
-                "$project": {
-                    "_id": 0,
-                    "value": "$_id",
-                    "count": 1
-                }
-            },
-            {
-                "$sort": {"count": -1}
-            }
-        ])
-        mongo_query = "db.Track.aggregate([{$group: {_id: '$Genre.Name', count: {$sum: 1}}, {$project: {_id: 0, Genre: '$_id', Count: 1}}, {$sort: {Count: -1}}])"
-        result_data = list(result)
-        return render_template('results_2.html', result_data=result_data, mongo_query=mongo_query, column_titles=["Genere", "Numero di Canzoni"], column_keys=["Genre", "count"])
+def count_albums():
+    return len(db.Track.distinct("Album.AlbumId")), ["Number of Albums"]
+
+def count_genres():
+    return len(db.Track.distinct("Genre.GenreId")), ["Number of Genres"]
+
+def count_artists():
+    return len(db.Track.distinct("Artist.ArtistId")), ["Number of Artists"]
+
+def count_playlists():
+    return len(db.Track.distinct("Playlist.PlaylistId")), ["Number of Playlists"]
+
+def count_songs_per_album():
+    return list(db.Track.aggregate([
+        {"$group": {"_id": "$Album.Title", "count": {"$sum": 1}}},
+        {"$project": {"_id": 0, "Album": "$_id", "Number of tracks": "$count"}},
+        {"$sort": {"Number of": -1}}
+    ])), None
+
+def count_songs_per_genre():
+    return list(db.Track.aggregate([
+        {"$group": {"_id": "$Genre.Name", "count": {"$sum": 1}}},
+        {"$project": {"_id": 0, "Genre": "$_id", "Number of tracks": "$count"}},
+        {"$sort": {"Number of tracks": -1}}
+    ])), None
+
+def count_songs_per_artist():
+    return list(db.Track.aggregate([
+        {"$group": {"_id": "$Artist.Name", "count": {"$sum": 1}}},
+        {"$project": {"_id": 0, "Artist": "$_id", "Count": "$count"}},
+        {"$sort": {"Count": -1}}
+    ])), None
+
+# Map the queries to functions
+queries = {
+    "Count the number of tracks": count_tracks,
+    "Count the number of albums": count_albums,
+    "Count the number of genres": count_genres,
+    "Count the number of artists": count_artists,
+    "Count the number of playlists": count_playlists,
+    "Count the number of songs per album": count_songs_per_album,
+    "Count the number of songs per genre": count_songs_per_genre,
+    "Count the number of songs per artist": count_songs_per_artist,
+}
+
+# Select the query
+selected_query = st.selectbox(
+    "Select the query to execute:",
+    options=list(queries.keys()),
+)
+
+# Esegui la query selezionata
+if selected_query:
+    st.write(f"Query selected: {selected_query}")
     
-    elif query_type == "songs_per_artist":
-        result = db.Track.aggregate([
-            {
-                "$group": {
-                    "_id": "$Artist.Name",
-                    "count": {"$sum": 1}
-                }
-            },
-            {
-                "$project": {
-                    "_id": 0,
-                    "value": "$_id",
-                    "count": 1
-                }
-            },
-            {
-                "$sort": {"count": -1}
-            }
-        ])
-        mongo_query = "db.Track.aggregate([{$group: {_id: '$Artist.Name', count: {$sum: 1}}, {$project: {_id: 0, Artist: '$_id', Count: 1}}, {$sort: {Count: -1}}])"
-        result_data = list(result)
-        return render_template('results_2.html', result_data=result_data, mongo_query=mongo_query, column_titles=["Artista", "Numero di Canzoni"], column_keys=["Artist", "count"])
-    elif query_type == "songs_per_playlist":
-        result = db.Track.aggregate([
-            {
-                "$unwind": "$Playlist"
-            },
-            {
-                "$group": {
-                    "_id": "$Playlist.Name",
-                    "count": {"$sum": 1}
-                }
-            },
-            {
-                "$project": {
-                    "_id": 0,
-                    "value": "$_id",
-                    "count": 1
-                }
-            },
-            {
-                "$sort": {"count": -1}
-            }
-        ])
-        mongo_query = "db.Track.aggregate([{$unwind: '$Playlist'}, {$group: {_id: '$Playlist.Name', count: {$sum: 1}}, {$project: {_id: 0, Playlist: '$_id', Count: 1}}, {$sort: {Count: -1}}])"
-        result_data = list(result)
-        return render_template('results_2.html', result_data=result_data, mongo_query=mongo_query, column_titles=["Playlist", "Numero di Canzoni"], column_keys=["Playlist", "count"])
-    elif query_type == "artists_per_playlist":
-        result = db.Track.aggregate([
-            {
-                "$unwind": "$Playlist"
-            },
-            {
-                "$group": {
-                    "_id": "$Playlist.Name",
-                    "artists": {"$addToSet": "$Artist.Name"}
-                }
-            },
-            {
-                "$project": {
-                    "_id": 0,
-                    "value": "$_id",
-                    "count": {"$size": "$artists"}
-                }
-            },
-            {
-                "$sort": {"count": -1}
-            }
-        ])
-        mongo_query = "db.Track.aggregate([{$unwind: '$Playlist'}, {$group: {_id: '$Playlist.Name', artists: {$addToSet: '$Artist.Name'}}, {$project: {_id: 0, Playlist: '$_id', Count: {$size: '$artists'}}}, {$sort: {Count: -1}}])"
-        result_data = list(result)
-        return render_template('results_2.html', result_data=result_data, mongo_query=mongo_query, column_titles=["Playlist", "Numero di Artisti"], column_keys=["Playlist", "count"])    
-    elif query_type == "albums_per_artist":
-        result = db.Track.aggregate([
-            {
-                "$unwind": "$Artist"
-            },
-            {
-                "$group": {
-                    "_id": "$Artist.Name",
-                    "albums": {"$addToSet": "$Album.Title"}
-                }
-            },
-            {
-                "$project": {
-                    "_id": 0,
-                    "value": "$_id",
-                    "count": {"$size": "$albums"}
-                }
-            },
-            {
-                "$sort": {"count": -1}
-            }
-        ])   
-        mongo_query = "db.Track.aggregate([{$unwind: '$Artist'}, {$group: {_id: '$Artist.Name', albums: {$addToSet: '$Album.Title'}}, {$project: {_id: 0, Artist: '$_id', Count: {$size: '$albums'}}}, {$sort: {Count: -1}}])"
-        result_data = list(result)
-        return render_template('results_2.html', result_data=result_data, mongo_query=mongo_query, column_titles=["Artista", "Numero di Album"], column_keys=["Artist", "count"]) 
+    query_result, titles = queries[selected_query]()  # Chiama la funzione corretta
+    
+    # Se il risultato è una lista (es. da aggregate()), visualizzalo come tabella
+    if isinstance(query_result, list):
+        st.dataframe(query_result)
     else:
-        result_data = []
-        mongo_query = "Invalid query type"
-    
-    return render_template('results.html', result_data=result_data, mongo_query=mongo_query)
+        st.write(f"{titles[0]}: {query_result}")
 
-if __name__ == "__main__":
-    app.run(debug=True)
