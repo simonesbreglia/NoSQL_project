d0c58b7da605083f0bbf191806d31f4a0b9bbfc7
Simone <286290@studenti.unimore.it>
2025-03-15 18:21:58 +0100

    Add function to retrieve and display external IP address

--- app.py
+++ app.py
@@ -5,6 +5,20 @@ from pymongo.server_api import ServerApi
 uri = st.secrets['mongo']['uri']
 client = MongoClient(uri, server_api=ServerApi('1'))
 
+import requests
+
+def get_external_ip():
+    response = requests.get("https://api64.ipify.org?format=json")
+    if response.status_code == 200:
+        data = response.json()
+        return data.get("ip")
+    else:
+        return "Unknown"
+
+external_ip = get_external_ip()
+st.write("External IP:", external_ip)
+
+
 db = client['Chinook']
 
 
