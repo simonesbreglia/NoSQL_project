ba4eb43f9da7f09d00cd7a7c8c4a96e0f8e68b4f
Simone Sbreglia <286290@studenti.unimore.it>
2025-04-03 13:56:26 +0200

    Add sales page enhancements and new query JSON for revenue analysis

--- my_app/pages/sales.py
+++ my_app/pages/sales.py
@@ -8,6 +8,16 @@ import plotly.graph_objects as go
 import pandas as pd
 import random
 
+def query_music(
+        db : MongoClient,
+        field : str,            # can be "Artist.Name", "Album.Title", "Track.Name", "Genre.Name"
+        selected_option : list, # list of artists, albums, tracks or genres
+        ):
+    # una query raccoglie le vendite totali per ogni oggetto selezionato
+    # una query raccoglie date di vendite e importo per ogni data
+
+    
+
 
 st.set_page_config(page_title="Sales Database", page_icon=":chart_with_upwards_trend:", layout="wide", initial_sidebar_state="collapsed")
 
@@ -35,6 +45,8 @@ fields = {
 
 def run_true():
     st.session_state.run = True
+def run_false():
+    st.session_state.run = False
 
 central_column_width = 0.4
 side_column_width = (1 - central_column_width) / 2
@@ -44,31 +56,45 @@ with central_column:
     decision = st.selectbox(
         label = "Start by choosing whether to view sales by employee, customer, or music",
         options = ("Sales by Employee", "Sales by Customer", "Sales by Music"),
-        index=None
+        index=None,
+        on_change=run_false
     )
     if decision == "Sales by Music":
         music_decision = st.selectbox(
             label = "Now you can choose weather to select a track, an album, an artist or a genre",
             options = ("Track", "Album", "Artist", "Genre"),
-            index=None
+            index=None,
+            on_change=run_false
         )
         if music_decision:
             options = db.Track.distinct(fields[music_decision]) if not None else []
             options = sorted(options)
+            options.insert(0, "Select All")
             selected_option = st.multiselect(
-                label = f"Select {music_decision}, select multiple option for comparing sales",
-                options = options)
+                label = f"Select {music_decision}, select multiple options for comparing sales",
+                options = options,
+                on_change=run_false,
+            )
+            if "Select All" in selected_option:
+                selected_option = options[1:]  # Exclude "Select All" from the selection
             
             if selected_option:
                 st.session_state.run = False
                 run = st.button(label = "Run Query", use_container_width=True)
-                st.write(run)
                 if run:
                     run_true()
 
 if st.session_state.run == True:
-    number = random.randint(1, 1000)
-    st.write(f"Query ID: {number}")
+    
+    if decision == "Sales by Music":
+        # grafico temporale per le vendite di ogni oggetto selezionato
+
+        if len(selected_option) > 1:
+            # aggiungo un istogramma per riassumere le vendite totali
+
+
+
+        
 
 
 
