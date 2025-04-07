0c82dd66108aaac2a072c970a6b58130ccdef084
Simone Sbreglia <286290@studenti.unimore.it>
2025-04-07 12:19:40 +0200

    cmplete employee sales

--- my_app/pages/sales.py
+++ my_app/pages/sales.py
@@ -8,10 +8,10 @@ from db_utils import get_local_connection, get_remote_connection, get_docker_con
 
 # get parent directory
 
-    
 
+st.set_page_config(page_title="Sales Database", page_icon=":chart_with_upwards_trend:", layout="wide",
+                   initial_sidebar_state="collapsed")
 
-st.set_page_config(page_title="Sales Database", page_icon=":chart_with_upwards_trend:", layout="wide", initial_sidebar_state="collapsed")
 
 def search_track_ids(_db, _selected_option, _music_decision):
     if _music_decision == "Track":
@@ -43,6 +43,23 @@ def search_track_ids(_db, _selected_option, _music_decision):
     return ids
 
 
+def search_employee_ids(_db, _selected_employees):
+    first_names = [
+        _ref.split(" ")[0] for _ref in _selected_employees
+    ]
+    last_names = [
+        _ref.split(" ")[1] for _ref in _selected_employees
+    ]
+
+    ids = _db.Employee.find(
+        {"FirstName": {"$in": first_names}, "LastName": {"$in": last_names}},
+        {"_id": 0, "Employee.EmployeeId": 1}
+    )
+
+    df_ids = pd.DataFrame(list(ids))
+    print(df_ids)
+    return ids
+
 
 path = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, os.pardir))
 
@@ -55,7 +72,6 @@ if "back" not in st.session_state:
 if "temporal_chart" not in st.session_state:
     st.session_state.temporal_chart = False
 
-
 st.markdown(
     """
     <h1 style='text-align: center;'>Sales Database</h1>
@@ -64,11 +80,11 @@ st.markdown(
 )
 
 fields = {
-           "Artist": "Artist.Name",
-           "Album": "Album.Title",
-           "Track": "Track.Name",
-            "Genre": "Genre.Name",
-        }
+    "Artist": "Artist.Name",
+    "Album": "Album.Title",
+    "Track": "Track.Name",
+    "Genre": "Genre.Name",
+}
 decision_map = {
     "Track": "$TrackName",
     "Album": "$AlbumName",
@@ -77,10 +93,10 @@ decision_map = {
 }
 
 
-
-
 def run_true():
     st.session_state.run = True
+
+
 def run_false():
     st.session_state.run = False
 
@@ -91,15 +107,15 @@ _, central_column, _ = st.columns([side_column_width, central_column_width, side
 
 with central_column:
     decision = st.selectbox(
-        label = "Start by choosing whether to view sales by employee, customer, or music",
-        options = ("Sales by Employee", "Sales by Customer", "Sales by Music"),
+        label="Start by choosing whether to view sales by employee, customer, or music",
+        options=("Sales by Employee", "Sales by Customer", "Sales by Music"),
         index=None,
         on_change=run_false
     )
     if decision == "Sales by Music":
         music_decision = st.selectbox(
-            label = "Now you can choose weather to select a track, an album, an artist or a genre",
-            options = ("Track", "Album", "Artist", "Genre"),
+            label="Now you can choose weather to select a track, an album, an artist or a genre",
+            options=("Track", "Album", "Artist", "Genre"),
             index=None,
             on_change=run_false
         )
@@ -108,32 +124,31 @@ with central_column:
             options = sorted(options)
             options.insert(0, "Select All")
             selected_option = st.multiselect(
-                label = f"Select {music_decision}, select multiple options for comparing sales",
-                options = options,
+                label=f"Select {music_decision}, select multiple options for comparing sales",
+                options=options,
                 on_change=run_false,
             )
             if "Select All" in selected_option:
                 selected_option = options[1:]  # Exclude "Select All" from the selection
 
             if selected_option:
-                print(selected_option)
                 st.session_state.run = False
-                run = st.button(label = "Run Query", use_container_width=True)
+                run = st.button(label="Run Query", use_container_width=True)
                 if run:
                     run_true()
 
     elif decision == "Sales by Employee":
         roles = db.Employee.distinct("Title")
         employee_role = st.multiselect(
-            label = "Select the role of the employee",
-            options = roles,
+            label="Select the role of the employee",
+            options=roles,
             on_change=run_false
         )
         if employee_role:
-            full_names = db.Employee.aggregate([
+            results = db.Employee.aggregate([
                 {
                     "$match": {
-                        "Title" :{
+                        "Title": {
                             "$in": employee_role
                         }
                     }
@@ -143,15 +158,18 @@ with central_column:
                         "Fullname": {
                             "$concat": ["$FirstName", " ", "$LastName", " - ", "$Title"]
                         },
+                        "EmployeeId": 1,
                         "_id": 0
                     }
                 }
             ])
-            full_names = [f["Fullname"] for f in full_names]
+            results = pd.DataFrame(list(results))
+            full_names = results["Fullname"].tolist()
+            ids_employee = results["EmployeeId"].tolist()
             full_names.insert(0, "Select All")
             selected_employees = st.multiselect(
-                label = "Select Employees",
-                options = full_names,
+                label="Select Employees",
+                options=full_names,
                 on_change=run_false
             )
             if "Select All" in selected_employees:
@@ -159,7 +177,7 @@ with central_column:
             if selected_employees:
                 selected_employees = [f.split(" - ")[0] for f in selected_employees]
 
-                run = st.button(label = "Run Query", use_container_width=True)
+                run = st.button(label="Run Query", use_container_width=True)
                 if run:
                     run_true()
 
@@ -168,19 +186,23 @@ if st.session_state.run == True:
         # grafico temporale per le vendite di ogni oggetto selezionato
         with open(path + f'/PipelinesMongoDB/SellPer{music_decision}.json') as f:
             pipeline = json.load(f)
+        track_ids = search_track_ids(db, selected_option, music_decision)
+        for stage in pipeline:
+            if "$match" in stage:
+                stage["$match"]["InvoiceLines.TrackId"]["$in"] = track_ids
+                break
         result = db.Invoice.aggregate(pipeline)
         df_result = pd.DataFrame(list(result))
         df_result.set_index("_id", inplace=True)
         df_result.index = df_result.index.str[0]
-
-
 
         # aggiungo un istogramma per riassumere le vendite totali
         indexes = df_result.index
         values = df_result.loc[:, "TotalRevenue"].values.flatten()
         if len(selected_option) < len(options):
             ref = {
-                indexes[i]: values[i] for i in range(len(indexes)) if indexes[i] for i in range(len(indexes)) if indexes[i] in selected_option
+                indexes[i]: values[i] for i in range(len(indexes)) if indexes[i] for i in range(len(indexes)) if
+                indexes[i] in selected_option
             }
             for opt in selected_option:
                 if opt not in list(ref.keys()):
@@ -218,8 +240,6 @@ if st.session_state.run == True:
 
         st.plotly_chart(bar_fig, use_container_width=True)
 
-
-
         with open(path + f'/PipelinesMongoDB/DatesInvoice{music_decision}.json') as f:
             pipeline = json.load(f)
         track_ids = search_track_ids(db, selected_option, music_decision)
@@ -233,20 +253,25 @@ if st.session_state.run == True:
         df_result_temporal["Date"] = pd.to_datetime(df_result_temporal["Date"])
         df_result_temporal.set_index([df_result_temporal.columns[0], df_result_temporal.columns[1]], inplace=True)
         # ordina le date
-        df_result_temporal.sort_index(level=[0,1], inplace=True)
+        df_result_temporal.sort_index(level=[0, 1], inplace=True)
         min_date = df_result_temporal.index.get_level_values(1).min()
         max_date = df_result_temporal.index.get_level_values(1).max()
-        print(min_date, max_date)
         date_range = pd.date_range(min_date, max_date, freq='D')
 
-        df_result_temporal.sort_index(level=[0,1], inplace=True)
+        df_result_temporal.sort_index(level=[0, 1], inplace=True)
         for genre in df_result_temporal.index.get_level_values(0).unique():
             missing_dates = date_range.difference(df_result_temporal.loc[genre].index)
-            df_result_temporal = pd.concat([df_result_temporal, pd.DataFrame({'TotalRevenue': 0.0},
-                                             index=pd.MultiIndex.from_product([[genre], pd.to_datetime(missing_dates)],
-                                                                              names=df_result_temporal.index.names))])
+            df_result_temporal = pd.concat([
+                df_result_temporal,
+                pd.DataFrame(
+                    {'TotalRevenue': 0.0},
+                    index=pd.MultiIndex.from_product([[genre],
+                                                      pd.to_datetime(
+                                                          missing_dates)],
+                                                     names=df_result_temporal.index.names))])
+
+        df_result_temporal.sort_index(level=[0, 1], inplace=True)
         df_result_temporal = df_result_temporal.groupby(level=0).resample('ME', level=1).sum()
-        df_result_temporal.sort_index(level=[0,1], inplace=True)
 
         line_plot = go.Figure()
         for option in selected_option:
@@ -258,7 +283,7 @@ if st.session_state.run == True:
                     go.Scatter(
                         x=indexes,
                         y=values,
-                        mode = "markers+lines",
+                        mode="markers+lines",
                         name=option,
                         line=dict(width=2),
                     )
@@ -277,18 +302,124 @@ if st.session_state.run == True:
             xaxis_title="Date",
             yaxis_title="Total Sales ($)",
             showlegend=True,
+            legend=dict(
+                orientation="h",
+                yanchor="bottom",
+                y=-0.5,
+                xanchor="center",
+                x=0.5
+            ),
         )
 
         st.plotly_chart(line_plot, use_container_width=True)
 
     elif decision == "Sales by Employee":
+        with open(path + '/PipelinesMongoDB/EmployeeSales.json') as f:
+            pipeline = json.load(f)
+        for stage in pipeline:
+            if "$match" in stage:
+                stage["$match"]["SupportRepId"]["$in"] = ids_employee
+                break
+
+        results = db.Invoice.aggregate(pipeline)
+        df_result = pd.DataFrame(list(results))
+        df_result['InvoiceDate'] = pd.to_datetime(df_result['InvoiceDate'])
+        df_result.set_index(['EmployeeId', 'InvoiceDate'], inplace=True)
+        df_result.sort_index(level=[0, 1], inplace=True)
+        min_date = df_result.index.get_level_values(1).min()
+        max_date = df_result.index.get_level_values(1).max()
+        date_range = pd.date_range(start=min_date, end=max_date, freq='D')
+
+        for employee_id in df_result.index.get_level_values(0).unique():
+            missing_dates = date_range.difference(df_result.loc[employee_id].index)
+            df_result = pd.concat([
+                df_result,
+                pd.DataFrame(
+                    {'TotalRevenue': 0.0},
+                    index=pd.MultiIndex.from_product(
+                        [[employee_id], pd.to_datetime(missing_dates)], names=df_result.index.names)
+                )
+            ])
+        if len(employee_role) > 1:
+            association = db.Employee.find(
+                {"EmployeeId": {"$in": ids_employee}},
+                {
+                    "_id": 0,
+                    "EmployeeId": 1,
+                    "Name": { "$concat": ["$FirstName", " ", "$LastName", " - ", "$Title"] }
+                }
+            )
+        else:
+            association = db.Employee.find(
+                {"EmployeeId": {"$in": ids_employee}},
+                {
+                    "_id": 0,
+                    "EmployeeId": 1,
+                    "Name": { "$concat": ["$FirstName", " ", "$LastName"] }
+                }
+            )
+        association = {
+            _ref['EmployeeId']: _ref['Name'] for _ref in association
+        }
 
 
 
+        df_result.sort_index(level=[0, 1], inplace=True)
+        df_result = df_result.groupby(level=0).resample('ME', level=1).sum()
 
+        bar_fig = go.Figure()
+        values = df_result.groupby(level = 0).sum().values.flatten()
+        indexes = [
+            association[_ref] for _ref in df_result.index.get_level_values(0).unique()
+        ]
+        bar_fig.add_trace(
+            go.Bar(
+                x = indexes,
+                y = values,
+                text = values,
+                textposition="auto",
+                textfont=dict(size=12),
+                width=0.5,
+            )
+        )
 
-        
+        st.plotly_chart(bar_fig, use_container_width=True)
 
+        # temporal chart
 
+        line_plot = go.Figure()
+        for employee in df_result.index.get_level_values(0).unique():
+            indexes = df_result.loc[pd.IndexSlice[employee, :], :].index.get_level_values(1).unique()
+            values = df_result.loc[pd.IndexSlice[employee, :], "TotalRevenue"].values.flatten()
 
-    
+            line_plot.add_trace(
+                go.Scatter(
+                    x=indexes,
+                    y=values,
+                    mode="markers+lines",
+                    name=association[employee],
+                    line=dict(width=2),
+                )
+            )
+
+        line_plot.update_layout(
+            title=dict(
+                text=f"Temporal Chart of Sales by Employee",
+                font=dict(size=20),
+                xanchor="center",
+                x=0.5,
+                yanchor="top",
+            ),
+            xaxis_title="Date",
+            yaxis_title="Total Sales ($)",
+            showlegend=True,
+            legend=dict(
+                orientation="h",
+                yanchor="bottom",
+                y=-0.5,
+                xanchor="center",
+                x=0.5
+            ),
+        )
+
+        st.plotly_chart(line_plot, use_container_width=True)
